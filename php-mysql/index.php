d19b347dec531380233cbc7cc91521dbb367379b
joelkun <47799679+joelkun@users.noreply.github.com>
2023-05-12 16:01:23 -0500

    conexion

---
+++ php-mysql/index.php
@@ -0,0 +1,40 @@
+<?php 
+// Conectar a la base de datos
+$conexion = mysqli_connect("localhost","root","","phpmysql");
+
+// Comprobar si la coneción es correcta
+if(mysqli_connect_errno()){
+	echo "La conexión a la base de datos MySQL ha fallado: ".mysqli_connect_error();
+}else{
+	echo "Conexión realizada correctamente!!";
+}
+
+// Consulta para configurar la codificacion de caracteres
+mysqli_query($conexion,"SET NAMES 'utf8'");
+
+// Consulta Select desde PHP
+$query=mysqli_query($conexion,"SELECT * FROM notas");
+//$notas=mysqli_fetch_assoc($query);
+
+while($nota=mysqli_fetch_assoc($query)){
+	//var_dump($nota);
+	echo "<h2>".$nota['titulo'].'</h2>';
+	echo $nota['descripcion'].'<br/>';
+
+}
+
+// Insertar en la base de datos desde PHP
+$sql="INSERT INTO notas VALUES (null,'Nota desde PHP','Esto es una nota desde PHP','green')";
+$insert=mysqli_query($conexion,$sql);
+
+echo "<hr/>";
+if($insert){
+	echo "DATOS INSERTADOS CORRECTAMENTE";
+}else{
+	echo "Error: ".mysqli_error($conexion);
+}
+
+
+
+
+ ?>
